2f38b41ee9ee23c48640e0d0d94e04c552500c15
Benjamin Noack <benjamin.noack@icloud.com>
2026-03-01 15:31:03 -0500

    Revert "optional creds to get past ci"
    This reverts commit b918ba7ef6943e6fde64f3e5a4e594d16e71d143.

--- src/shared/common/schemas/env.ts
+++ src/shared/common/schemas/env.ts
@@ -3,7 +3,7 @@ import { LogLevelSchema } from "../../log/schemas/logger.js";
 
 export const EnvSchema = z
   .object({
-    HELPER_COINBASE_CREDENTIALS_PATH: z.string().min(1).optional(),
+    HELPER_COINBASE_CREDENTIALS_PATH: z.string().min(1),
     HELPER_POSTGRES_DATABASE: z.string().min(1).optional(),
     HELPER_POSTGRES_USERNAME: z.string().min(1).optional(),
     HELPER_POSTGRES_PASSWORD: z.string().min(1).optional(),
