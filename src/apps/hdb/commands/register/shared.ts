3c39cf5633943aab2abf73512e2bfab2ebd6cad8
Benjamin Noack <benjamin.noack@icloud.com>
2026-03-01 10:44:33 -0500

    remove unnecessary files

--- src/apps/hdb/commands/register/shared.ts
+++
@@ -1,1 +0,0 @@
-export { runAction as handleAction, runActionWithArgument as handleActionWithArgument } from "../shared/action-runner.js";
--- src/apps/hdb/db/orders.ts
+++
@@ -1,1 +0,0 @@
-export * from "./coinbase/orders/repository.js";
